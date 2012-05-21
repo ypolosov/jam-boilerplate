168f068041914f2aa06f3e10dfdda243a149218c
Aleksey Ivanov <stupidlogin@gmail.com>
2012-05-22 00:53:09 +0600

    grunt config modified

--- build/grunt.js
+++ build/grunt.js
@@ -4,21 +4,19 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // the staging directory used during the process
-    staging: 'intermediate/',
+    staging: 'intermediate',
     // final build output
-    output: 'publish/',
-
+    output: 'publish',
     // filter any files matching one of the below pattern during mkdirs task
     // the pattern in the .gitignore file should work too.
     exclude: '.git* build/** node_modules/** grunt.js package.json *.md'.split(' '),
     mkdirs: {
-      staging: '<config:exclude>',
-      output: '<config:exclude>'
+      staging: '<config:exclude>'
     },
     // concat css/**/*.css files, inline @import, output a single minified css
-    css: {
-      'blocks/__style.css': ['blocks/**/*.css']
-    },
+    // css: {
+    //   'blocks/__style.css': ['blocks/**/*.css']
+    // },
     watch: {
       scripts: {
         files: '<config:lint.files>',
@@ -30,20 +28,34 @@ module.exports = function(grunt) {
       }
     },
     styletto: {
-      config: {
-        input: "blocks/style.css",
-        output: "blocks/__style.css",
+      dev: {
+        src: "blocks/style.css",
+        dest: "blocks/__style.css",
+        compress: "csso",
+        base64: 15000,
+        resolveFrom: ""
+      },
+      publish: {
+        src: "blocks/style.css",
+        dest: "blocks/__style.css",
         compress: "csso",
         base64: 15000,
         resolveFrom: ""
       }
     },
     csslint: {
+      base_theme: {
         src: "blocks/**/*.css",
         rules: {
-            // "import": false,
-            "overqualified-elements": 2
+            "import": false,
+            "overqualified-elements": 2,
+            "unique-headings": false,
+            "star-property-hack": false,
+            "known-properties": false,
+            "box-sizing": false,
+            "qualified-headings": false
         }
+      }
     },
     // Renames JS/CSS to prepend a hash of their contents for easier
     // versioning
@@ -109,6 +121,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-styletto');
   grunt.loadNpmTasks('grunt-css');
 
-  grunt.registerTask('default', 'styletto');
-  grunt.registerTask('publish', 'csslint styletto concat min img'); // not tested
+  // grunt.registerTask('default', 'styletto');
+  grunt.registerTask('default', 'intro clean mkdirs concat csslint styletto min');
+  grunt.registerTask('publish', 'intro clean mkdirs styletto concat min img'); // not tested, rev usemin?
+
+  grunt.registerTask('reload', 'default connect watch:reload');
+
 };
